a4dd0d6659252bdec2e4982122ca35e9d2064444
Kenan Babačić <babacic@bk.ru>
2021-07-22 14:59:08 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -958,7 +958,7 @@
                                                             echo "moodle-icon";
                                                             break;
                                                         case "educational";
-                                                            echo "mortarboard";
+                                                            echo "video-icon";
                                                             break;
                                                     } ?>">
                     <span class="blog-icon-description"><?php switch ($blog->tag) {
@@ -1014,7 +1014,7 @@
                                                         echo "moodle-icon";
                                                         break;
                                                     case "educational";
-                                                        echo "mortarboard";
+                                                        echo "video-icon";
                                                         break;
                                                 } ?>">
                     <span class="blog-icon-description"><?php switch ($blog->tag) {
@@ -1077,7 +1077,7 @@
                                                     break;
                                                 case "educational";
 
-                                                    echo "mortarboard";
+                                                    echo "video-icon";
                                                     break;
                                             } ?>">
                 <span class="blog-icon-description"><?php switch ($blog->tag) {
